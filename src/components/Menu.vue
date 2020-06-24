87a4e44340432f8432bbc16d0dec1ffe7ecee2ff
Lickitung <brandon_n_evans@live.com>
2020-06-24 13:19:42 -0500

    loop through menu items

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,60 +1,115 @@
 <template>
-    <div class="menu_wrapper">
-        <!-- menu -->
-        <div class="menu">
-            <h3>~ Authentic handmade pizza ~</h3>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>
-                            <strong>~ Pepperoni ~</strong>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, enim.</small>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>9"</td>
-                        <td>$6.95</td>
-                        <td>
-                            <button type="button" class="btn_green">+</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+  <div class="menu_wrapper">
+    <!-- menu -->
+    <div class="menu">
+      <h3>~ Authentic handmade pizza ~</h3>
+      <table v-for="item in getMenuItems" :key="item.name">
+        <tbody>
+          <tr>
+            <td>
+              <strong>~ {{ item.name }} ~</strong>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <small>{{ item.description }}</small>
+            </td>
+          </tr>
+          <tr v-for="(option, index) in item.options" :key="option[index]">
+            <td>{{ option.size }}"</td>
+            <td>${{ option.price }}</td>
+            <td>
+              <button type="button" class="btn_green">+</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 
 <style scoped>
 h3 {
-    text-align: center;
+  text-align: center;
 }
 
 .menu-wrapper {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .menu {
-    background: #f1e6da;
-    border-radius: 3px;
-    height: 100vh;
-    margin: 10px;
-    padding: 10px;
+  background: #f1e6da;
+  border-radius: 3px;
+  height: 100vh;
+  margin: 10px;
+  padding: 10px;
 }
 
 @media screen and (min-width: 900px) {
-    .menu-wrapper {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-    }
+  .menu-wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
 
-    .menu {
-        width: 65vw;
-    }
+  .menu {
+    width: 65vw;
+  }
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      getMenuItems: {
+        1: {
+          name: "Margherita",
+          description: "A delicious tomato based pizza topped with mozzarella",
+          options: [
+            {
+              size: 9,
+              price: 6.95
+            },
+            {
+              size: 12,
+              price: 10.95
+            }
+          ]
+        },
+        2: {
+          name: "Pepperoni",
+          description:
+            "A delicious tomato based pizza topped with mozzarella and pepperoni",
+          options: [
+            {
+              size: 9,
+              price: 7.95
+            },
+            {
+              size: 12,
+              price: 12.95
+            }
+          ]
+        },
+        3: {
+          name: "Ham and Pineapple",
+          description:
+            "A delicious tomato based pizza topped with mozzarella, ham and pineapple",
+          options: [
+            {
+              size: 9,
+              price: 7.95
+            },
+            {
+              size: 12,
+              price: 12.95
+            }
+          ]
+        }
+      }
+    };
+  }
+};
+</script>
