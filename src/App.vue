22063516172274736f642a0759ecbe2302afbd70
Lickitung <brandon_n_evans@live.com>
2020-06-24 13:03:01 -0500

    add Menu component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
     <AppHeader />
     <Home />
+    <Menu />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
 import Home from './components/Home'
+import Menu from './components/Menu'
+
 
 export default {
   name: 'App',
   components: {
     AppHeader: Header,
-    Home
+    Home,
+    Menu
   }
 }
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="menu_wrapper">
+        <!-- menu -->
+        <div class="menu">
+            <h3>~ Authentic handmade pizza ~</h3>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <strong>~ Pepperoni ~</strong>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, enim.</small>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>9"</td>
+                        <td>$6.95</td>
+                        <td>
+                            <button type="button" class="btn_green">+</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h3 {
+    text-align: center;
+}
+
+.menu-wrapper {
+    display: flex;
+    flex-direction: column;
+}
+
+.menu {
+    background: #f1e6da;
+    border-radius: 3px;
+    height: 100vh;
+    margin: 10px;
+    padding: 10px;
+}
+
+@media screen and (min-width: 900px) {
+    .menu-wrapper {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    .menu {
+        width: 65vw;
+    }
+}
+</style>
