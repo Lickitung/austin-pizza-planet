473970b2ce696aab7dd7dbe0e3f18eb1058305d9
Lickitung <brandon_n_evans@live.com>
2020-06-24 12:48:18 -0500

    add Home component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
   <div id="app">
-    <app-header />
+    <AppHeader />
+    <Home />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Home from './components/Home'
 
 export default {
   name: 'App',
   components: {
-    appHeader: Header
+    AppHeader: Header,
+    Home
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
